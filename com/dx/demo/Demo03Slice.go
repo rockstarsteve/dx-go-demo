e3fe435d4af2fca195a6bf8a7ec941af9b8526e8
DESKTOP-T8CPIPR\rockstarsteve <670139644@qq.com>
2022-01-05 23:44:11 +0800

    测试

--- com/dx/demo/Demo03Slice.go
+++ com/dx/demo/Demo03Slice.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
+
+	//切片不存值
 
 	var arrray []int
 
@@ -35,5 +40,38 @@ func main() {
 	arrray5 := arrray2[:]
 
 	fmt.Println("array5:", arrray5, "长度：", len(arrray5), "容量是：", cap(arrray5))
+
+	fmt.Println("====================================================")
+
+	var a1 = []int{1, 2, 3, 4, 5}
+	fmt.Println("a1:", a1)
+	fmt.Println("a1  type:", reflect.TypeOf(a1))
+
+	var a2 = [...]int{1, 2, 3, 4, 5}
+	fmt.Println("a2:", a2)
+
+	fmt.Println("a2  type:", reflect.TypeOf(a2))
+
+	fmt.Println("====================================================")
+
+	var b1 = []int{1, 2, 3, 4, 5}
+	b2 := b1[:]
+
+	b3 := b2[2:]
+	b2 = b3
+
+	fmt.Println("b3:", b3)
+
+	fmt.Println("b1:", b1)
+
+	b2 = append(b2[:1], b2[3:]...)
+
+	fmt.Println("b1:", b1)
+
+	fmt.Println("====================================================")
+
+	var t = make([]string, 5, 10)
+
+	fmt.Println("a:", t, "cap:", cap(t), "len:", len(t))
 
 }
